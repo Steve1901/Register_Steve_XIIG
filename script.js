c0b5ac5aaa05678676681262244495170ca76a4a
Steve <137023876+Steve1901@users.noreply.github.com>
2025-08-21 11:34:03 +0700

    Add files via upload
    script yang ada alert nya

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 document.getElementById("registerform").addEventListener("submit", function(e){
         e.preventDefault();
         
+        //ambil nilai dari input
         const name = document.getElementById("Name").value;
         const email = document.getElementById("Email").value;
         const password = document.getElementById("Password").value;
         const noHp = document.getElementById("noHp").value;
         const tanggalLahir = document.getElementById("TanggalLahir").value;
         
+        // buat simpan di local storage
+        const user = {
+            name: name,
+            email: email,
+            password: password,
+            noHp: noHp,
+            tanggalLahir: tanggalLahir
+        };
+        localStorage.setItem(email, JSON.stringify(user));
+        alert("Sukses mantap isi dua");
+        
+        // masukin data ke tabel secara otomatis
         const table = document.getElementById("datatable").getElementsByTagName("tbody")[0];
         const newRow = table.insertRow();
+        
         
         newRow.insertCell(0).textContent = name;
         newRow.insertCell(1).textContent = email;
